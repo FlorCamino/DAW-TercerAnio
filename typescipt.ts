e0378f1a0ee18441ef7582c54a086c211501e9fc
Franco Challiol <francochalliol23@gmail.com>
2026-04-06 19:26:36 -0300

    Agrego punto 6 y un ejemplo y agrego punto7

--- typescipt.ts
+++ typescipt.ts
@@ -5,3 +5,27 @@ function saludar(nombre: string): string {
 }   
 
 console.log(saludar(nombreUsuario));
+
+// Punto 6
+enum DiasSemana {
+  Lunes = "Lunes",
+  Martes = "Martes",
+  Miercoles = "Miercoles",
+  Jueves = "Jueves",
+  Viernes = "Viernes",
+  Sabado = "Sabado",
+  Domingo = "Domingo"
+}
+
+// Ejemplo del punto 6
+let hoy: DiasSemana = DiasSemana.Miercoles;
+console.log("Hoy es:", hoy);
+
+// Punto 7
+let dato: string | number;
+
+dato = "Messi";
+console.log("Valor string:", dato);
+
+dato = 10;
+console.log("Valor number:", dato);
