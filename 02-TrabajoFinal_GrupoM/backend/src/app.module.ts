316ae8dabf1c1e3130842cb3e34ce67f36bfa678
Franco Challiol <francochalliol23@gmail.com>
2026-05-18 23:13:56 -0300

    Agregue tasksmodule

--- 02-TrabajoFinal_GrupoM/backend/src/app.module.ts
+++ 02-TrabajoFinal_GrupoM/backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { databaseConfig } from './config/database.config';
 import { ClientsModule } from './modules/clients/clients.module';
 import { ProjectsModule } from './modules/projects/projects.module';
+import { TasksModule } from './modules/tasks/tasks.module';
 
 @Module({
   imports: [
@@ -13,7 +14,8 @@ import { ProjectsModule } from './modules/projects/projects.module';
     }),
     TypeOrmModule.forRoot(databaseConfig()),
     ClientsModule,
-    ProjectsModule
+    ProjectsModule,
+    TasksModule,
   ],
   controllers: [AppController],
 })
