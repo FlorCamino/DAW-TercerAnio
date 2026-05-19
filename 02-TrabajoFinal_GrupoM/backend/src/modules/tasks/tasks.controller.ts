30bb0819a12a1dec25674ccdcd24605b611fca82
Franco Challiol <francochalliol23@gmail.com>
2026-05-18 23:11:49 -0300

    Agregue contenido en tasks.controller

---
+++ 02-TrabajoFinal_GrupoM/backend/src/modules/tasks/tasks.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+
+import { CreateTaskDto } from './dtos/input/create-task.dto';
+import { UpdateTaskDto } from './dtos/input/update-task.dto';
+import { TasksService } from './tasks.service';
+
+@ApiTags('tasks')
+@Controller('tasks')
+export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
+  // GET /tasks
+  // GET /tasks?status=Pendiente
+  @Get()
+  findAll(@Query('status') status?: string) {
+    return this.tasksService.findAll(status);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.tasksService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() dto: CreateTaskDto) {
+    return this.tasksService.create(dto);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() dto: UpdateTaskDto) {
+    return this.tasksService.update(id, dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.tasksService.remove(id);
+  }
+}
