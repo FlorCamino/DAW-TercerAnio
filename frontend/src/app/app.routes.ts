da79b2a1c402bb2038e128b83a9a99148f2296d9
Florencia Camino <flopiis.1994@gmail.com>
2026-05-23 19:44:13 +0200

    cambio de directorio frontend janet - agregue en buscador “cliente no encontrado”

--- frontend/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { ClientesListComponent } from './features/clients/components/clientes-list/clientes-list.component';
-import { HomeComponent } from './features/home/components/home/home.component';
-
-export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'clientes', component: ClientesListComponent },
-];
