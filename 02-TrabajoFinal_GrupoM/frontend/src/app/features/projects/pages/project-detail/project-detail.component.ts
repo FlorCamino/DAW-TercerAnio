4016b67e0214c00df07e801c8e238b32739affcf
damianottone <dami.ottone@gmail.com>
2026-05-30 18:14:29 -0300

    agrego tareas del proyecto en vista de detalle

--- 02-TrabajoFinal_GrupoM/frontend/src/app/features/projects/pages/project-detail/project-detail.component.ts
+++ 02-TrabajoFinal_GrupoM/frontend/src/app/features/projects/pages/project-detail/project-detail.component.ts
@@ -4,6 +4,9 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 import { ProjectStatusBadgeComponent } from '../../components/project-status-badge/project-status-badge.component';
 import { Project } from '../../models/project.model';
 import { ProjectService } from '../../services/project.service';
+import { TasksService } from '../../../tasks/services/tasks.service';
+import { Task } from '../../../tasks/models/task.model';
+
 
 @Component({
   selector: 'app-project-detail',
@@ -14,9 +17,11 @@ import { ProjectService } from '../../services/project.service';
 })
 export class ProjectDetailComponent implements OnInit {
   project = signal<Project | null>(null);
+  tasks = signal<Task[]>([]);
 
   constructor(
     private readonly projectService: ProjectService,
+    private readonly tasksService: TasksService,
     private readonly route: ActivatedRoute,
   ) { }
 
@@ -25,6 +30,10 @@ export class ProjectDetailComponent implements OnInit {
 
     this.projectService.getOne(id).subscribe((data: Project) => {
       this.project.set(data);
+    });
+
+    this.tasksService.getTareas({ proyectoId: id }).subscribe(data => {
+      this.tasks.set(data);
     });
   }
 }
